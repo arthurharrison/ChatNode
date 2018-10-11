79d07d79d10258e0223e1765510c3cf0e4e5363e
Arthur Tavares <tavares593@gmail.com>
2018-10-11 00:42:05 -0300

    Login page is done

---
+++ public/js/screen.js
@@ -0,0 +1,38 @@
+let socket = null;
+
+document.getElementById("btnGo").onclick = () => {
+    const name = document.getElementById('userText').value;
+    document.getElementById('userText').value = ''
+    document.getElementById('loginIn').style.display = 'none';
+    document.getElementById('chatter').style.display = 'block';
+    $.ajax({
+        url: 'http://localhost:3000/change',
+        method:'POST',
+        data: {name: name}
+      }).done(function(){
+        socket = io.connect();
+        
+        $("form#chat").submit(function(e) {
+            e.preventDefault();
+            socket.emit("send message", $(this).find("userText").val() + " - " + $(this).find("#msg_text").val(), function() {
+                $("form#chat #msg_text").val("");
+            });
+        });
+  
+        socket.on('chat message', function(msg){
+          //handle the message however you would like
+          $('#history').append($('<p />').text(msg));
+  
+        });
+  
+        socket.on("update messages", function(msg){
+          var final_message = $("<p />").text(msg);
+          $("#history").append(final_message);
+        });
+   
+        console.log('deu certo');
+      }).fail(function(){
+        console.log('deu ruim');
+      });
+
+}
